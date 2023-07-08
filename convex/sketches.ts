060394e511e5a69288b05094747b9a9cd9b82406
s <96222718+sa6ds@users.noreply.github.com>
2023-07-08 00:25:20 -0400

    ability to write and fetch sketches

---
+++ convex/sketches.ts
@@ -0,0 +1,19 @@
+import { mutation, query } from "./_generated/server";
+
+export const saveSketch = mutation(
+  async ({ db }, { prompt }: { prompt: string }) => {
+    console.log(prompt);
+    await db.insert("sketches", {
+      prompt,
+    });
+
+    return {
+      message: "success",
+    };
+  }
+);
+
+export const getSketches = query(async ({ db }) => {
+  const sketches = await db.query("sketches").collect();
+  return sketches;
+});

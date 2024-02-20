e3aa37f5da4ef28911cfc6eac9f9ff1011792f8d
s <96222718+sa6ds@users.noreply.github.com>
2024-02-20 00:29:57 -0500

    update to newest convex version

--- convex/generate.ts
+++ convex/generate.ts
@@ -1,22 +1,21 @@
-
-
-
 "use node";
 
+import { v } from "convex/values";
+import { internal } from "./_generated/api";
 import { internalAction } from "./_generated/server";
 import Replicate from "replicate";
 
-export const generate = internalAction(
-  async (
-    { runMutation },
-    {
-      prompt,
-      image,
-      sketchId,
-    }: { sketchId: string; prompt: string; image: string }
-  ) => {
+export const generate = internalAction({
+  args: { sketchId: v.id("sketches"), prompt: v.string(), image: v.string() },
+  handler: async (ctx, { prompt, image, sketchId }) => {
+    if (!process.env.REPLICATE_API_TOKEN) {
+      throw new Error(
+        "Add REPLICATE_API_TOKEN to your environment variables: " +
+          "https://docs.convex.dev/production/environment-variables"
+      );
+    }
     const replicate = new Replicate({
-      auth: process.env.REPLICATE_API_TOKEN!,
+      auth: process.env.REPLICATE_API_TOKEN,
     });
 
     const output = (await replicate.run(
@@ -33,9 +32,9 @@ export const generate = internalAction(
       }
     )) as [string, string];
 
-    await runMutation("sketches:updateSketchResult", {
+    await ctx.runMutation(internal.sketches.updateSketchResult, {
       sketchId,
       result: output[1],
     });
-  }
-);
+  },
+});
--- convex/sketches.ts
+++ convex/sketches.ts
@@ -1,19 +1,16 @@
-import { Id } from "./_generated/dataModel";
+import { internal } from "./_generated/api";
 import { internalMutation, mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
 export const saveSketch = mutation({
-  args: {
-    prompt: v.string(),
-    image: v.string(),
-  },
-  handler: async ({ db, scheduler }, { prompt, image }) => {
-    const sketch = await db.insert("sketches", {
+  args: { prompt: v.string(), image: v.string() },
+  handler: async (ctx, { prompt, image }) => {
+    const sketch = await ctx.db.insert("sketches", {
       prompt,
     });
 
-    await scheduler.runAfter(0, "generate:generate", {
-      sketchId: sketch.id,
+    await ctx.scheduler.runAfter(0, internal.generate.generate, {
+      sketchId: sketch,
       prompt,
       image,
     });
@@ -22,24 +19,26 @@ export const saveSketch = mutation({
   },
 });
 
-export const getSketch = query(({ db }, { sketchId }: { sketchId: string }) => {
-  if (!sketchId) return null;
-  return db.get(new Id("sketches", sketchId));
+export const getSketch = query({
+  args: { sketchId: v.id("sketches") },
+  handler: (ctx, { sketchId }) => {
+    if (!sketchId) return null;
+    return ctx.db.get(sketchId);
+  },
 });
 
-export const updateSketchResult = internalMutation(
-  async (
-    { db },
-    { sketchId, result }: { sketchId: string; result: string }
-  ) => {
-    const id = new Id("sketches", sketchId);
-    await db.patch(id, {
+export const updateSketchResult = internalMutation({
+  args: { sketchId: v.id("sketches"), result: v.string() },
+  handler: async (ctx, { sketchId, result }) => {
+    await ctx.db.patch(sketchId, {
       result,
     });
-  }
-);
+  },
+});
 
-export const getSketches = query(async ({ db }) => {
-  const sketches = await db.query("sketches").collect();
-  return sketches;
+export const getSketches = query({
+  handler: async (ctx) => {
+    const sketches = await ctx.db.query("sketches").collect();
+    return sketches;
+  },
 });
